8730fbb639b8fa6a85a71ea4a2f2c16177cc0c49
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-02 11:35:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx';
+import Footer from './Footer';
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <Footer></Footer>
+    </>
+    
+  );
+  
+}
+
+export default App
